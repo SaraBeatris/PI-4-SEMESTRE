381ca18dd84dd6c9d1003dff70db2e7174d394fc
Sara Beatriz Bento <167379586+SaraBeatris@users.noreply.github.com>
2025-09-20 19:39:28 -0300

    Update settings.py

--- carona/carona/settings.py
+++ carona/carona/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'usuarios','veiculos', 'templates')],
 
         'APP_DIRS': True,
         'OPTIONS': {
